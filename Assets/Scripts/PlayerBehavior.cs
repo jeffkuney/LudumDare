461caadae544e033f12af3b37af6be71bc680cbc
Josh Brown <josh@sjoshbrown.com>
2019-04-26 22:05:23 -0700

    add playercontroller

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float MovementSpeed = 1.0f;
+    public float JumpHeight = 1.0f;
+    public float MaxVelocity = 1.0f;
+
+    private Rigidbody2D _rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        var velocity = new Vector3();
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity = velocity.normalized;
+
+        transform.Translate(velocity * MovementSpeed);
+    }
+}
