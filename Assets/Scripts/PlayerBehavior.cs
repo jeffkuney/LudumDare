f417e290cd5d06d62228061f660059e139debd62
Josh Brown <josh@sjoshbrown.com>
2019-04-27 08:35:12 -0700

    Jumping Main Man

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public float MovementSpeed = 1.0f;
-    public float JumpHeight = 1.0f;
+    public float JumpVelocity = 1.0f;
     public float MaxVelocity = 1.0f;
+    public float SpeedLoss = .1f;
 
     private Rigidbody2D _rigidBody;
+    private Vector2 _velocity = new Vector2();
 
 
     // Start is called before the first frame update
@@ -28,12 +30,29 @@ public class PlayerBehavior : MonoBehaviour
         
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _velocity.y = 0.0f;
+    }
+
     private void FixedUpdate()
     {
-        var velocity = new Vector3();
-        velocity.x = Input.GetAxis("Horizontal");
-        velocity = velocity.normalized;
+        var horizontal = Input.GetAxis("Horizontal");
+        bool jump = Input.GetButton("Jump");
 
-        transform.Translate(velocity * MovementSpeed);
+        _velocity += new Vector2(horizontal, 0.0f);
+        _velocity.x = Mathf.Lerp(_velocity.x, 0, SpeedLoss);
+
+        if (jump)
+        {
+            Debug.Log("jumping");
+            _velocity.y += JumpVelocity;
+            jump = false;
+        }
+
+        _velocity.x = Mathf.Clamp(_velocity.x, -MaxVelocity, MaxVelocity);
+        _velocity.y = Mathf.Clamp(_velocity.y, -MaxVelocity, MaxVelocity);
+
+        transform.Translate(_velocity * MovementSpeed);
     }
 }
